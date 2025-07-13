34c3014e0f427e2eac8b2207f29e38d7ff7249dc
natzgun <itzlilouxx@gmail.com>
2025-07-13 15:35:43 -0500

    Now we can vizualizate correctly

--- src/main.cc
+++ src/main.cc
@@ -148,17 +148,28 @@ void render(const int vertexCount) {
 
   glm::mat4 model = glm::mat4(1.0f);
 
-  model =
-      glm::rotate(model, glm::radians(rotationX), glm::vec3(1.0f, 0.0f, 0.0f));
-  model =
-      glm::rotate(model, glm::radians(rotationY), glm::vec3(0.0f, 1.0f, 0.0f));
+  model = glm::rotate(model,
+    glm::radians(rotationX),
+    glm::vec3(1.0f, 0.0f, 0.0f)
+  );
+  model = glm::rotate(model,
+    glm::radians(rotationY),
+    glm::vec3(0.0f, 1.0f, 0.0f)
+  );
 
   // model = glm::scale(model, glm::vec3(1.0f));
 
-  glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f),
-                               glm::vec3(0.0f, 1.0f, 0.0f));
-  glm::mat4 proj =
-      glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+  glm::mat4 view = glm::lookAt(
+    glm::vec3(0.0f, 0.0f, 100.0f),
+    glm::vec3(0.0f),
+    glm::vec3(0.0f, 1.0f, 0.0f)
+  );
+
+  glm::mat4 proj = glm::perspective(
+    glm::radians(45.0f),
+    800.0f / 600.0f,
+    0.1f, 1000.0f
+  );
 
 // glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 500.0f), glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 // glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 1000.0f);
